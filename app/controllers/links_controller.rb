75b24305f64821d7e317dbba65277020678da4ba
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-09-25 09:12:53 -0400

    Create controller and views for Link model

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,20 @@
+class LinksController < ApplicationController
+  def index
+    @links = Link.recent_first
+  end
+
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      redirect_to links_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+    def link_params
+      params.require(:link).permit(:url)
+    end
+  end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,6 @@
 class Link < ApplicationRecord
+  validates :url, presence: true
+
+  scope :recent_first, -> { order(created_at: :desc) }
+
 end
