61a808bc930cbc1705175b535850d9830409f57f
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-09-29 12:41:30 -0400

    Switch to RSpec

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -2,6 +2,10 @@ class ViewsController < ApplicationController
   before_action :set_link
 
   def show
+    @link.views.create!(
+      ip: request.ip,
+      user_agent: request.user_agent,
+    )
     # Because we're using the `allow_other_host` option, we can redirect to any URL (external)
     redirect_to @link.url, allow_other_host: true
   end
